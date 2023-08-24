ca6487500c1162383662065d3bddb055f930f3b8
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-08-24 17:31:59 -0300

    Refactor: Adicionado rotas na aplicação e refatorado app

--- src/App.vue
+++ src/App.vue
@@ -4,13 +4,7 @@
       <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
-      <FormularioTarefa @aoSalvarTarefa="salvarTarefa"/>
-      <div class="lista">
-        <TarefaComponent v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
-        <BoxComponent v-if="listaEstaVazia">
-          Nenhuma tarefa cadastrada.
-        </BoxComponent>
-      </div>
+      <router-view></router-view>
     </div>
   </main>
 </template>
@@ -18,34 +12,18 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from './components/BarraLateral.vue';
-import FormularioTarefa from './components/FormularioTarefa.vue';
-import TarefaComponent from './components/TarefaComponent.vue';
-import BoxComponent from './components/BoxComponent.vue';
-import { ITarefa } from './components/interfaces/ITarefa';
 
 export default defineComponent({
   name: 'App',
   components: {
     BarraLateral,
-    FormularioTarefa,
-    TarefaComponent,
-    BoxComponent
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
-  computed: {
-    listaEstaVazia(): boolean {
-      return this.tarefas.length === 0;
-    }
-  },
   methods: {
-    salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa)
-    },
     trocarTema(modoEscuroAtivo: boolean) {
       this.modoEscuroAtivo = modoEscuroAtivo;
     }
---
+++ src/views/Tarefas.vue
@@ -0,0 +1,41 @@
+<template>
+  <FormularioTarefa @aoSalvarTarefa="salvarTarefa"/>
+  <div class="lista">
+    <TarefaComponent v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+    <BoxComponent v-if="listaEstaVazia">
+      Nenhuma tarefa cadastrada.
+    </BoxComponent>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import FormularioTarefa from '../components/FormularioTarefa.vue';
+import TarefaComponent from '../components/TarefaComponent.vue';
+import BoxComponent from '../components/BoxComponent.vue';
+import { ITarefa } from '../components/interfaces/ITarefa';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    FormularioTarefa,
+    TarefaComponent,
+    BoxComponent
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+    }
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0;
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    },
+  },
+});
+</script>
