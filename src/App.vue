8da0dc69a1e13cdf0ecb94c5644fc6d4174c445f
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2023-08-24 15:50:26 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
+    <div class="column is-one-quarter">
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
+    </div>
+    <div class="column is-three-quarter conteudo">
+      <FormularioTarefa @aoSalvarTarefa="salvarTarefa"/>
+      <div class="lista">
+        <TarefaComponent v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa"/>
+        <BoxComponent v-if="listaEstaVazia">
+          Nenhuma tarefa cadastrada.
+        </BoxComponent>
+      </div>
+    </div>
+  </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import BarraLateral from './components/BarraLateral.vue';
+import FormularioTarefa from './components/FormularioTarefa.vue';
+import TarefaComponent from './components/TarefaComponent.vue';
+import BoxComponent from './components/BoxComponent.vue';
+import { ITarefa } from './components/interfaces/ITarefa';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    BarraLateral,
+    FormularioTarefa,
+    TarefaComponent,
+    BoxComponent
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
+    }
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0;
+    }
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa)
+    },
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo;
+    }
+  },
 });
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .lista {
+    padding: 1.25rem;
+  }
+  :root {
+    --bg-primario: #fff;
+    --texto-primario: #000;
+  }
+  .modo-escuro {
+    --bg-primario: #2b2d42;
+    --texto-primario: #ddd;
+  }
+  .conteudo {
+    background-color: var(--bg-primario);
+  }
 </style>
